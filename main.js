b2d8725df61f0e09f36bf24ea26be46bb220ca41
Travis <tsahadeo@mac.com>
2022-03-22 16:44:12 -0400

    commit tree d009550d8dda157682150dc678ff03981b3d2930

--- main.js
+++ main.js
@@ -1,8 +1,53 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector("#modal")
 
 // Your JavaScript code goes here!
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Dom Content Has Loaded")
+  errorModal.classList.add("hidden")
+
+  clickListener()
+})
+
+function findLikes(){
+  const likeArr = document.querySelectorAll(".like-glyph")
+
+  likeArr.forEach((singularLike) => {
+    singularLike.addEventListener("click", () => console.log("Hola, como esta?"))
+  })
+}
+
+function hideError() {
+  errorModal.classList.add("hidden")
+}
+function clickListener(){
+  document.addEventListener('click', (event) => {
+    if(event.target.classList[0] === 'like-glyph'){
+      mimicServerCall()
+      .then(resp => {
+        //console.log(event.target.innerHTML);
+        const activated = event.target.classList.contains("activated-heart");
+        if(activated){
+          event.target.classList.remove("activated-heart")
+          event.target.innerHTML = EMPTY_HEART;
+        }
+        else {
+          event.target.classList.add("activated-heart");
+          event.target.innerHTML = FULL_HEART;
+        }
+      })
+      .catch(error => {
+        setTimeout(() => {
+          hideError()
+        }, 3000)
+      })
+    }
+  })
+}
+
 
 
 
